e7b348cd3f7c1b1fffc3c4a06d4894fc061de805
rajan9661 <240634@kit.ac.in>
2026-03-07 01:52:17 +0530

    Create odd_even_linkedlist.cpp
    Added odd even linked list solution

---
+++ odd_even_linkedlist.cpp
@@ -0,0 +1,17 @@
+
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if(head==NULL || head->next==NULL) return head;
+    ListNode *odd=head;
+    ListNode *even=head->next;
+    ListNode *maintain=head->next;
+      while(even!=NULL && even->next!=NULL) {
+        odd->next=even->next;
+        odd=odd->next;
+        even->next=odd->next;
+        even=even->next;
+      }
+      odd->next=maintain;
+      return head;
+    }
